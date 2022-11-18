72cfb8240859bde615da788d8624838b51a7bb7d
Nataliaplm <nataliapalomof@gmail.com>
2022-11-18 13:35:32 +0100

    take code from branch indexJS to indexjsv2

---
+++ js/index.js
@@ -0,0 +1,34 @@
+//Botón play
+const playButton = document.querySelector('#playButton');
+
+playButton.addEventListener('click', startPlay);
+
+function startPlay(){
+    
+};
+
+
+//Array participantes
+let listCoders = [
+    {name: 'Natalia', status: 'on'},
+    {name: 'Paula', status: 'on'},
+    {name: 'Veronika', status: 'on'},
+    {name: 'Lola García', status: 'on'},
+    {name: 'Adriana', status: 'on'},
+    {name: 'Raquel', status: 'on'}
+];
+console.log(listCoders)
+
+
+
+
+//Seleccionar coder
+
+
+
+
+//Pop up
+
+//Listado inverso
+
+//Añadir participante
