89d1ba7e91d2fedafcc2987c20a89acd4d0a6f05
Adriana Aguilar <agruad@gmail.com>
2022-11-21 16:02:20 +0100

    implemented random, splice and unshift methods

--- js/index.js
+++ js/index.js
@@ -2,20 +2,73 @@
 const playButton = document.querySelector('#playButton');
 
 //le decimos a la constante que cuando se haga click en JUGAR ejecute la funcion startPlay
-playButton.addEventListener('click', startPlay());
+playButton.addEventListener('click', startPlay);
 
 //Array participantes con nombre y sin estado ON/OFF
 let listCoders = ['Natalia', 'Paula', 'Veronika','Lola G', 'Adriana','Raquel'];
-/* let randomCoder = listCoders[Math.floor(Math.random()* listCoders.length)];
-console.log(randomCoder); */
 
-// función que ejecuta el botón JUGAR: creamos variable con List COders vivas ON que sea aleatoria con Math random
+//Array participantes eliminadas
+let listCodersOff = [];
+
+// función que ejecuta el botón JUGAR: creamos variable con List Coders vivas ON que sea aleatoria con Math random
 function startPlay(){
-   
+    let randomCoder = Math.floor(Math.random() * listCoders.length);
+    
+    let selectedCoders = listCoders.splice(randomCoder, 1); // sacar de la lista de vivos -> guardado en una variable
+    if (listCoders.length === 0) {
+        swal("¡Juega de nuevo!", {
+        buttons: false,
+        timer: 1500,
+    });
+    }
 
-    //console.log("le has dado");
-   
+    console.log(selectedCoders);
+    console.log(listCoders);
+    swal(`La Coder eliminada es ${selectedCoders}`, {
+        buttons: false,
+        timer: 1500,
+    });
+    //Lista de eliminadas
+    let deletedCoders = listCodersOff.unshift(selectedCoders);
+    deletedCoders = listCodersOff.toString();
+    console.log(deletedCoders);
+    //console.log(listCodersOff);
+    //si la lista esta vacia para el juego
+    //let newArray = listCodersOff.toString();
+    //console.log(newArray);
 };
+
+/* movieSection.innerHTML += `
+<div class="card" style="width: 18rem;">
+    <img src="${img}" class="card-img-top" alt="Imagen de la película">
+    <div class="card-body">
+        <h3 class="card-title">${id}. ${title}</h3>
+        <h4 class="card-title">Director: ${director}</h4>
+        <h5 class="card-title">Género: ${genre}</h5>
+    </div>
+</div>
+` */
+
+coders.innerHTML += `
+    <div id="coders" class="participants">
+        <div class="row">
+            <div class="col">${listCoders[0]}</div>
+            <div class="col">${listCoders[1]}</div>
+            <div class="col">${listCoders[2]}</div>
+            <div class="col">${listCoders[3]}</div>
+            <div class="col">${listCoders[4]}</div>
+            <div class="col">${listCoders[5]}</div>
+        </div>
+    </div>
+`
+codersOff.innerHTML += `
+    <div id="codersOff" class="container">
+        <img src="./images/monitor.png" alt="ordenador" width=80%">
+        <div class="listCoders">
+            <p>${listCodersOff}</p><br>
+        </div>
+    </div>
+`
 
 // variable con listado de coders eliminadas
 
